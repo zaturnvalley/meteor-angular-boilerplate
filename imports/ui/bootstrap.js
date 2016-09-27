4cd6f55a9dba5ee29a1f545c45b6c3464d211388
zaturnvalley <zacharycmason@gmail.com>
2016-09-27 15:27:55 -0700

    injected Home into bootstrap.js

--- imports/ui/bootstrap.js
+++ imports/ui/bootstrap.js
@@ -2,13 +2,19 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
 
+import { name as Home } from './pages/home/home';
+
 import template from './bootstrap.html';
 
 class Bootstrap {}
 
 const name = 'bootstrap';
 
-export default angular.module(name, [angularMeteor, uiRouter])
+export default angular.module(name, [
+  angularMeteor, 
+  uiRouter,
+  Home
+  ])
 .component(name, {
   template, 
   controllerAs: name,
