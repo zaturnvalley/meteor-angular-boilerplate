db6c9d139626147a4039ce6eead50dc139af5ea0
zaturnvalley <zacharycmason@gmail.com>
2016-09-27 14:30:33 -0700

    onready for bootstrap, angular, cordova

--- client/main.js
+++ client/main.js
@@ -1,1 +1,14 @@
-import angular from 'angular'
+import angular from 'angular';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { Meteor } from 'meteor/meteor';
+
+function onReady() {
+  angular.bootstrap(document, [], {strictDi: true});
+}
+
+if(Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}
